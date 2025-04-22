b04c21326975c7e2503d1843c2d1de7bda2058cd
misu <daum091409@naver.com>
2025-04-22 13:29:20 +0900

    ff

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        OPENAI_API_KEY: process.env.OPENAI_API_KEY, // 환경 변수 추가
-      },
-};
+  // ──<< 이 한 줄이 핵심입니다 >>──
+  output: "export",
 
+};
 export default nextConfig;
